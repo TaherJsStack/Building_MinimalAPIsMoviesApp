748a8933a905b1084c959321917f739cd4b4222a
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-19 12:22:59 +0200

    Add Delete method to IGenresRepositories for genre removal by ID

--- Repositories/IGenresRepositories.cs
+++ Repositories/IGenresRepositories.cs
@@ -9,5 +9,6 @@ namespace Building_MinimalAPIsMoviesApp.Repositories
         Task<List<Genre>> GetAll();
         Task<bool> Exists(int id);
         Task Update(Genre genre);
+        Task Delete(int id);
     }
 }
