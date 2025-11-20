8f0ef262db1f53859f149527a240481319c1347f
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-11-20 07:12:29 +0200

    acc actors dtos classes

---
+++ DTOs/CreateActorDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace Building_MinimalAPIsMoviesApp.DTOs
+{
+    public class CreateActorDTO
+    {
+        public string Name { get; set; } = null!;
+        public DateTime DateOfBirth { get; set; }
+        public IFormFile? Picture { get; set; }
+    }
+}
