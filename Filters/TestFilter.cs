5e423f09c4ae814011819fcbddbc4f460dd8eb89
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 14:41:39 +0200

    Add TestFilter class to implement endpoint filtering, allowing pre- and post-processing of endpoint requests for enhanced control over request handling.

---
+++ Filters/TestFilter.cs
@@ -0,0 +1,15 @@
+﻿
+namespace Building_MinimalAPIsMoviesApp.Filters
+{
+    public class TestFilter : IEndpointFilter
+    {
+        public async ValueTask<object?> InvokeAsync(EndpointFilterInvocationContext context, EndpointFilterDelegate next)
+        {
+            //this is the code that will execute Before the endpoint
+            var results = await next(context);
+            //this is the code that will execute After the endpoint
+
+            return results;
+        }
+    }
+}
