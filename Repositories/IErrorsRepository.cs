29d6f81a3f545ab4e8028753d7d7e9372adcc8cc
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 22:15:56 +0200

    Add IErrorsRepository interface to define error creation functionality in the repository layer

---
+++ Repositories/IErrorsRepository.cs
@@ -0,0 +1,9 @@
+﻿using Building_MinimalAPIsMoviesApp.Entities;
+
+namespace Building_MinimalAPIsMoviesApp.Repositories
+{
+    public interface IErrorsRepository
+    {
+        Task Create(Error error);
+    }
+}
