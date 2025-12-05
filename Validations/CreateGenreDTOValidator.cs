4dc5ce7024bbfbae975578b46b13f806a715af8c
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 10:35:17 +0200

    Refactor CreateGenreDTOValidator to include asynchronous genre existence check using IGenresRepository

--- Validations/CreateGenreDTOValidator.cs
+++ Validations/CreateGenreDTOValidator.cs
@@ -1,11 +1,12 @@
 ﻿using Building_MinimalAPIsMoviesApp.DTOs;
+using Building_MinimalAPIsMoviesApp.Repositories;
 using FluentValidation;
 
 namespace Building_MinimalAPIsMoviesApp.Validations
 {
-    public class CreateGenreDTOValidator: AbstractValidator<CreateGenreDTO>
+    public class CreateGenreDTOValidator : AbstractValidator<CreateGenreDTO>
     {
-        public CreateGenreDTOValidator()
+        public CreateGenreDTOValidator(IGenresRepository genresRepository)
         {
             RuleFor(p => p.Name)
                 .NotEmpty()
@@ -14,16 +15,20 @@ namespace Building_MinimalAPIsMoviesApp.Validations
                 .WithMessage("Custom Maximum Length Message Error For {PropertyName}")
                 .Must(FirstLitterIsUppercase)
                 .WithMessage("First Litter Is Uppercase Error Message")
+                .MustAsync(async (name, _) =>
+                {
+                    var exists = await genresRepository.Exists(id: 0, name);
+                    return !exists;
+                }).WithMessage(g => $"a genre with the {g.Name} already exists.")
                 ;
         }
 
-        private bool FirstLitterIsUppercase(string value) 
-        { 
+        private bool FirstLitterIsUppercase(string value)
+        {
             if (string.IsNullOrWhiteSpace(value))
             {
                 return true;
-            };
-
+            }
             var firestLitter = value[0].ToString();
             return firestLitter == firestLitter.ToUpper();
         }
