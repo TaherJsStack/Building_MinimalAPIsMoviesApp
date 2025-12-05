dd46b7e520eda6744dd66b119887bf9229cea23a
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 17:07:56 +0200

    Add CreateMovieDTOValidator for movie validation, ensuring non-empty titles with length restrictions for improved request validation.

---
+++ Validations/CreateMovieDTOValidator.cs
@@ -0,0 +1,17 @@
+﻿using Building_MinimalAPIsMoviesApp.DTOs;
+using FluentValidation;
+
+namespace Building_MinimalAPIsMoviesApp.Validations
+{
+    public class CreateMovieDTOValidator : AbstractValidator<CreateMovieDTO>
+    {
+        public CreateMovieDTOValidator()
+        {
+            RuleFor(p => p.Title)
+                .NotEmpty()
+                .WithMessage(ValidationUtilities.NotEmptyMessage)
+                .MaximumLength(150)
+                .WithMessage(ValidationUtilities.MaximumLengthMessage);
+        }
+    }
+}
