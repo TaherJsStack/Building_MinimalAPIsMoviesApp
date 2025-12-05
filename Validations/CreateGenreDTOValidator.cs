edbd9ee17017c6930d153ea2453ccb7b6c00ab8c
Taher Ahmed <Taher.Ahmed@softec.ai>
2025-12-05 10:47:03 +0200

    Update CreateGenreDTOValidator to include IHttpContextAccessor for route value extraction, enabling ID parsing from the request context.

--- Validations/CreateGenreDTOValidator.cs
+++ Validations/CreateGenreDTOValidator.cs
@@ -6,8 +6,16 @@ namespace Building_MinimalAPIsMoviesApp.Validations
 {
     public class CreateGenreDTOValidator : AbstractValidator<CreateGenreDTO>
     {
-        public CreateGenreDTOValidator(IGenresRepository genresRepository)
+        public CreateGenreDTOValidator(IGenresRepository genresRepository, IHttpContextAccessor httpContextAccessor)
         {
+            var routeValueId = httpContextAccessor.HttpContext.Request.RouteValues["id"];
+            var id = 0;
+
+            if (routeValueId is string routeValueIdString)
+            {
+                int.TryParse(routeValueIdString, out id );
+            }
+
             RuleFor(p => p.Name)
                 .NotEmpty()
                 .WithMessage("Custom Not Empty Message Error For {PropertyName}")
